acb86bfa72ebde22bb0e293479851778f2f64ec9
LimMinKyo <story1400@naver.com>
2023-05-20 15:23:18 +0900

    ADD: Pretendard 폰트 적용
    Signed-off-by: LimMinKyo <story1400@naver.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,37 @@
+import localFont from '@next/font/local';
+import 'styles/globals.css';
+
+const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  style: 'normal',
+  weight: '45 920',
+  fallback: [
+    'Pretendard Variable',
+    'Pretendard',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.className}>
       <body>{children}</body>
     </html>
   );
