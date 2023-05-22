9bc57883bb1def54f625050698326f21b767184a
LimMinKyo <story1400@naver.com>
2023-05-22 15:13:07 +0900

    ADD: GoogleTagManager 컴포넌트
    Signed-off-by: LimMinKyo <story1400@naver.com>

---
+++ components/GoogleTagManager.tsx
@@ -0,0 +1,31 @@
+import Script from 'next/script';
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
+export default function GoogleTagManager() {
+  return GTM_ID ? (
+    <>
+      {/* <!-- Google Tag Manager --> */}
+      <Script id="gtm-init" strategy="afterInteractive">{`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}</Script>
+      {/* <!-- End Google Tag Manager --> */}
+
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript>
+        <iframe
+          title="gtm-noscript"
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
+    </>
+  ) : null;
+}
