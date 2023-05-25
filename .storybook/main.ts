856886689903ea25342bbd2d249d3c36592c6f06
LimMinKyo <story1400@naver.com>
2023-05-25 15:46:34 +0900

    UPT: Setting Storybook
    Signed-off-by: LimMinKyo <story1400@naver.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,8 @@
 import path from 'path';
-
 import type { StorybookConfig } from '@storybook/react-webpack5';
+
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|ts)'],
   addons: [
     path.dirname(
       require.resolve(path.join('@storybook/addon-links', 'package.json')),
@@ -15,15 +15,18 @@ const config: StorybookConfig = {
         path.join('@storybook/addon-interactions', 'package.json'),
       ),
     ),
+    path.dirname(
+      require.resolve(path.join('@storybook/addon-styling', 'package.json')),
+    ),
   ],
   framework: {
-    name: path.dirname(
-      require.resolve(path.join('@storybook/react-webpack5', 'package.json')),
-    ),
+    // @ts-ignore
+    name: '@storybook/nextjs',
     options: {},
   },
   docs: {
     autodocs: 'tag',
   },
 };
+
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,11 @@
 import type { Preview } from '@storybook/react';
+import '../styles/globals.css';
+
+import Image from 'next/image';
+
+Image.defaultProps = {
+  unoptimized: true,
+};
 
 const preview: Preview = {
   parameters: {
